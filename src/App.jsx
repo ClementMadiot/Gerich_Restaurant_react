3f850b6d00e3c3de0ca86a75caf250ffbebcbe45
ClementMadiot <clementmadiot09@gmail.com>
2024-05-29 15:32:05 +0200

    add component + composent + css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,30 @@
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from './container'
+import { Navbar } from './components'
+import './App.css'
 
-export default function App(){
-return 'hi'
-}
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+)
+
+export default App
