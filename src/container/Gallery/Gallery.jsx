1c16a105da7c622b9a3a099d3663c5a4a8288652
ClementMadiot <clementmadiot09@gmail.com>
2024-06-01 14:09:52 +0200

    import dynamically gallery

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import {
   BsInstagram,
   BsArrowLeftShort,
@@ -9,16 +9,50 @@ import { SubHeading } from '../../components'
 import { images } from '../../constants'
 import './Gallery.css'
 
+const galleryImages = [images.gallery01,images.gallery02,images.gallery03,images.gallery04]
+
 const Gallery = () => {
+  const scrollRef = useRef(null)
+  const scroll = (direction) => {
+    const { current } = scrollRef
 
-
+    if (direction === 'let') {
+      current.scrollRef -= 300
+    } else {
+      current.scrollRef += 300
+    }
+  }
   return (
-    <section className='app_gallery flex_center'>
-      <div className="app_gallery-content">
-        <SubHeading title="photo gallery" subtitle="Instagram"/>
-        <p className="p_opensans" style={{ color: "#AAA", marginTop: '2rem'}}></p>
-      </div>
-
+    <section className="app_gallery flex_center">
+      <article className="app_gallery-content">
+        <SubHeading title="photo gallery" subtitle="Instagram" />
+        <p className="p_opensans" style={{ color: '#AAA', marginTop: '2rem' }}>
+          Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Volutpat
+          Mattis Ipsum Turpis Elit Elit Scelerisque Egestas Mu.
+        </p>
+        <button type="button" className="custom_button">
+          View More
+        </button>
+      </article>
+      <article className="app_gallery-images">
+        <div className="app_gallery-images_container" ref={scrollRef}>
+        {galleryImages.map((image,index) => (
+          <div key={`gallery-image-${index + 1}`} className="app_gallery-images_card flex_center">
+            <img src={image} alt={`gallery-${index}`} />
+          </div>
+        ))}
+        </div>
+        <div className="app_gallery-images_arrow">
+          <BsArrowLeftShort
+            className="gallery_arrow-icon"
+            onClick={() => scroll('left')}
+          />
+          <BsArrowRightShort
+            className="gallery_arrow-icon"
+            onClick={() => scroll('right')}
+          />
+        </div>
+      </article>
     </section>
   )
 }
