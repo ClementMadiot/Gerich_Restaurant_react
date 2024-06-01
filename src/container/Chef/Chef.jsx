03cf350b650d80b8e887f47d429e58291c8f3287
ClementMadiot <clementmadiot09@gmail.com>
2024-06-01 09:32:46 +0200

    add chef section with his css

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from 'react'
 
-import './Chef.css';
+import { SubHeading } from '../../components'
+import images from '../../constants/images'
+import './Chef.css'
 
 const Chef = () => (
-  <div>
-    Chef
-  </div>
-);
+  <section className="app_bg app_wrapper section_padding">
+    <div className="app_wrapper_img app_wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
 
-export default Chef;
+    <div className="app_wrapper_info">
+      <SubHeading title="chef's word" />
+      <h1 className="headtext_cormorant">What We Believe In</h1>
+
+      <div className="app_chef-content">
+        <div className="app_chef-content_quote">
+          <img className='tamere' src={images.quote} alt="quote" />
+          <p className="p_opensans">
+            Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit Auctor Sit .
+          </p>
+        </div>
+        <p className="p_opensans">
+          Auctor Sit Iaculis In Arcu. Vulputate Nulla Lobortis Mauris Eget Sit.
+          Nulla Scelerisque Scelerisque Congue Ac Consequat, Aliquam Molestie
+          Lectus Eu. Congue Iaculis Integer Curabitur Semper Sit Nunc.
+        </p>
+      </div>
+      <div className="app_chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p_opensans">Chef & Founder</p>
+        <img src={images.sign} alt="chef's signature" />
+      </div>
+    </div>
+  </section>
+)
+
+export default Chef
