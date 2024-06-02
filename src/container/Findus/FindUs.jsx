048aa8076a24c5a5b993f82a1b327e5b9538f131
ClementMadiot <clementmadiot09@gmail.com>
2024-06-02 08:58:54 +0200

    add find us section + logo

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from 'react'
 
+import { SubHeading } from '../../components'
+import { images } from '../../constants'
 const FindUs = () => (
-  <div>
-    FindUs
-  </div>
-);
+  <section id="contact" className="app_bg app_wrapper section_padding">
+    <div className="app_wrapper_info">
+      <SubHeading
+        title="find us"
+        subtitle="Contact"
+        style={{ marginBottom: '3rem' }}
+      />
+      <div className="app_wrapper-content">
+        <p className="opensans">Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 75G</p>
+        <p className="p_cormorant" style={{color: '#DCCA87', margin: '2rem 0'}}>Opening Hours</p>
+        <p className="opensans">Mon - Fri: 10:00 Am - 2:00 Am</p>
+        <p className="opensans">Sat - Sun: 10:00 Am - 3:00 Am</p>
+      </div>
+      <button className="custom_button" style={{marginTop:'2rem'}}>Visit Us</button>
+    </div>
+    <div className="app_wrapper_img">
+      <img src={images.findus} alt="lemonade" />
+    </div>
+  </section>
+)
 
-export default FindUs;
+export default FindUs
