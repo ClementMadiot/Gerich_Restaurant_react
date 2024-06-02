eeb4c9d19f11b1e15348194ff8a4ad854a73f997
ClementMadiot <clementmadiot09@gmail.com>
2024-06-02 11:43:15 +0200

    add footer component

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { SubHeading } from '../../components'
+import { images } from '../../constants'
 import './FooterOverlay.css';
 
 const FooterOverlay = () => (
-  <div>
-    FooterOverlay
-  </div>
+  <article className='app_footerOverlay'>
+    <div className="app_footerOverlay-black"></div>
+    <div className="app_footerOverlay-img app_bg"></div>
+  </article>
 );
 
 export default FooterOverlay;
--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import  SubHeading  from '../SubHeading/SubHeading'
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
-  </div>
+  <article className='app_newsletter'>
+    <div className="app_newsletter-heading">
+      <SubHeading title="subscribe to our newsletter" subtitle="Newsletter"/>
+      <p className="p_opensans">And Never Miss Latest Updated!</p>
+    </div>
+    <form action='submit' className="app_newsletter-input flex_center">
+      <input type="email" placeholder='Enter your e-mail'/>
+      <button className="custom_button">Subscribe</button>
+    </form>
+  </article>
 );
 
 export default Newsletter;
